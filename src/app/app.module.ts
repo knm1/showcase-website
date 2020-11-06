066563692df4160d710002de32a36735e5a3f977
Renaud Wellens <rwellens@gmail.com>
2020-11-06 17:48:20 +0100

    add crapy cookies consent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,38 @@ import {ContactComponent} from './partials/contact/contact.component';
 import {ProjectsComponent} from './projects/projects.component';
 import {ProjectComponent} from './projects/project/project.component';
 import {NgxPageScrollModule} from 'ngx-page-scroll';
+import {NgcCookieConsentConfig, NgcCookieConsentModule} from 'ngx-cookieconsent';
+
+const cookieConfig: NgcCookieConsentConfig = {
+  'cookie': {
+    'domain': 'tinesoft.github.io'
+  },
+  'position': 'bottom',
+  'theme': 'edgeless',
+  'palette': {
+    'popup': {
+      'background': '#161c2d',
+      'text': '#ffffff',
+      'link': '#ffffff'
+    },
+    'button': {
+      'background': '#3259d0',
+      'text': '#000000',
+      'border': 'transparent'
+    }
+  },
+  'type': 'info',
+  'content': {
+    'message': 'This website uses cookies to ensure you get the best experience on our website.',
+    'dismiss': 'Got it!',
+    'deny': 'Decline',
+    'link': 'Learn more',
+    'href': 'https://knm.lu',
+    'policy': 'Cookie Policy',
+    'header': 'Cookies used on the website!',
+    'allow': 'Allow cookies'
+  }
+};
 
 
 const APP_ROUTE: Routes = [
@@ -40,6 +72,7 @@ const routerOptions: ExtraOptions = {
   imports: [
     BrowserModule,
     RouterModule.forRoot(APP_ROUTE, routerOptions),
+    NgcCookieConsentModule.forRoot(cookieConfig),
     NgxPageScrollModule
   ],
   providers: [],
