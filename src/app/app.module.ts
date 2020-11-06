943774a1413160a0f465f7793d0d3bdb4a17c7e3
Renaud Wellens <rwellens@gmail.com>
2020-11-06 16:40:02 +0100

    add router and page scroll

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,49 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {ExtraOptions, RouterModule, Routes} from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ModalsComponent } from './partials/modals/modals.component';
-import { HomeComponent } from './home/home.component';
-import { FooterComponent } from './partials/footer/footer.component';
-import { NavbarComponent } from './partials/navbar/navbar.component';
-import { ContactComponent } from './partials/contact/contact.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {FooterComponent} from './partials/footer/footer.component';
+import {NavbarComponent} from './partials/navbar/navbar.component';
+import {ContactComponent} from './partials/contact/contact.component';
+import {ProjectsComponent} from './projects/projects.component';
+import {ProjectComponent} from './projects/project/project.component';
+import {NgxPageScrollModule} from 'ngx-page-scroll';
+
+
+const APP_ROUTE: Routes = [
+  {path: 'company', component: HomeComponent},
+  {path: 'project/karstate', component: ProjectComponent},
+  {path: 'project/knm-platform', component: ProjectComponent},
+  {
+    path: '',
+    redirectTo: '/company',
+    pathMatch: 'full'
+  }
+];
+
+const routerOptions: ExtraOptions = {
+  anchorScrolling: 'enabled',
+};
 
 @NgModule({
   declarations: [
     AppComponent,
-    ModalsComponent,
     HomeComponent,
     FooterComponent,
     NavbarComponent,
-    ContactComponent
+    ContactComponent,
+    ProjectsComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(APP_ROUTE, routerOptions),
+    NgxPageScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
