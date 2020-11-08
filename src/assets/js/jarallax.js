f8855c00a99c167902855eaa55ed3e4d5b77d6cb
Renaud Wellens <rwellens@gmail.com>
2020-11-08 10:38:42 +0100

    add text inner blockcode

--- src/assets/js/jarallax.js
+++ src/assets/js/jarallax.js
@@ -2,4 +2,15 @@ $(document).ready(function () {
   $('[data-jarallax-element]').jarallax({
     speed: 0.2
   });
+
+  var typed = new Typed('#typed',
+    {
+      typeSpeed: 50,
+      strings: [
+        "&gt; $ curl -i karstate.com<br/><span class=\"text-success\">Get all your reliable data</span><br/>" +
+        "<br/>" +
+        "&gt; $ that&#x2019;s it<br/>" +
+        "<span class=\"text-success\">Yup, that&#x2019;s it.</span>"]
+    }
+  );
 });
